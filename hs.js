976730338963631249df68c063cf09a8de8460e5
abdulrfarag <ar060290@gmail.com>
2023-01-28 14:16:53 -0600

    getting storage out of the arrey

--- hs.js
+++ hs.js
@@ -4,5 +4,6 @@ renderTable();
 function renderTable() {
   // clear out the TableBody
   tableBody.innerHTML = "";
-}
+  // get the array out of storage
+  const arrayFromStorage = JSON.parse(localStorage.getItem("scoreArray"));
 
